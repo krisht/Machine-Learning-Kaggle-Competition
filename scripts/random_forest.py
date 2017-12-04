fa0d0dbd43d86f7664b3baedbddb6e880f7664ee
Krishna Thiyagarajan <krisht@cooper.edu>
2017-12-04 17:52:01 -0500

    Submitted Code

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.base import clone
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda, QuadraticDiscriminantAnalysis as qda
 from sklearn.ensemble import AdaBoostClassifier, ExtraTreesClassifier, RandomForestClassifier, VotingClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.svm import libsvm_sparse
 from sklearn.manifold import TSNE
 from sklearn.utils import shuffle
@@ -12,11 +12,7 @@ INPUT_PATH = "../inputs/train.csv"
 TEST_PATH = "../inputs/test.csv"
 
 def read_data(path, header=1):
-	if header is None:
-		data = pd.read_csv(path)
-	else:
-		data = pd.read_csv(path)
-	return data
+	return pd.read_csv(path)
 
 def split_data_set():
 	data_set = read_data(INPUT_PATH)
@@ -24,10 +20,10 @@ def split_data_set():
 	data_set = shuffle(data_set)
 	sets = np.split(data_set, [200], 0)
 
-	valid_y = np.asarray([sets[0]['label']]).T
+	valid_y = np.asarray(sets[0]['label'])
 	valid_X = np.asarray(sets[0].loc[:, sets[0].columns != 'label'])
 
-	train_y = np.asarray([sets[1]['label']]).T
+	train_y = np.asarray(sets[1]['label'])
 	train_X = np.asarray(sets[1].loc[:, sets[1].columns != 'label'])
 
 	test_X = np.asarray(test_data)[:, 1:]
@@ -35,18 +31,33 @@ def split_data_set():
 	return train_X, valid_X, test_X, train_y, valid_y
 
 
+train_X, valid_X, test_X, train_y, valid_y = split_data_set()
+
+clf = RandomForestClassifier(n_jobs=-1)
+
+from scipy.stats import randint as sp_randint
+param_dist = {"n_estimators": [10, 100, 1000, 10000], "max_depth": [None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+              "max_features": sp_randint(1, 11),
+              "min_samples_split": sp_randint(2, 11),
+              "min_samples_leaf": sp_randint(1, 11),
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+
+n_iter_search = 20
+clf = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=n_iter_search)
 
 
-train_X, valid_X, test_X,  train_y, valid_y = split_data_set()
-
-clf = lda(solver='eigen', n_components=0, shrinkage=0.10710710710710711)
 
 clf.fit(train_X, train_y)
-print(clf.score(train_X, train_y))
-print(clf.score(valid_X, valid_y))
-with open('out.csv', 'w') as f:
-	f.write('Id,Prediction\n')
+print(clf.best_score_)
+print(clf.best_params_)
 
-
-np.savetxt(open('out.csv', 'ab'), np.c_[np.arange(1, len(test_X)+1), clf.predict(test_X)], '%d', delimiter=',')
-#print(clf.predict(test_X))
+# print(clf.score(train_X, train_y))
+# print(clf.score(valid_X, valid_y))
+#
+# with open('out.csv', 'w') as f:
+# 	f.write('Id,Prediction\n')
+#
+#
+# np.savetxt(open('out.csv', 'ab'), np.c_[np.arange(1, len(test_X)+1), clf.predict(test_X)], '%d', delimiter=',')
+#
